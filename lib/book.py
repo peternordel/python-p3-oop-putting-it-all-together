85a2f7e5c57b5ed54b83f17847ed9e271f9c5ff7
Peter Nordel <peternordel@gmail.com>
2023-05-02 17:10:40 -0600

    complete lab

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,51 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title = "title", author = "author", page_count = 0, genre = "genre"):
+        self.title = title
+        self.author = author
+        self.page_count = page_count
+        self.genre = genre
+    
+    def get_title(self):
+        print('Retrieving title')
+        return self._title
+
+    def set_title(self, title):
+        self._title = title
+
+    title = property(get_title, set_title)
+
+    def get_author(self):
+        print('Retrieving author')
+        return self._author
+
+    def set_author(self, author):
+        self._author = author
+
+    author = property(get_author, set_author)
+
+    def get_page_count(self):
+        print('Retrieving page_count')
+        return self._page_count
+
+    def set_page_count(self, page_count):
+        if type(page_count) == int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    page_count = property(get_page_count, set_page_count)
+
+    def get_genre(self):
+        print('Retrieving genre')
+        return self._genre
+
+    def set_genre(self, genre):
+        self._genre = genre
+
+    genre = property(get_genre, set_genre)
+
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,61 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand = "brand", color = "color", size = 0, material = "material", condition = "condition"):
+        self.brand = brand
+        self.color = color
+        self.size = size
+        self.material = material
+        self.condition = condition
+    
+    def get_brand(self):
+        print('Retrieving brand')
+        return self._brand
+
+    def set_brand(self, brand):
+        self._brand = brand
+
+    brand = property(get_brand, set_brand)
+
+    def get_color(self):
+        print('Retrieving color')
+        return self._color
+
+    def set_color(self, color):
+        self._color = color
+
+    color = property(get_color, set_color)
+
+    def get_size(self):
+        print('Retrieving size')
+        return self._size
+
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    size = property(get_size, set_size)
+
+    def get_material(self):
+        print('Retrieving material')
+        return self._material
+
+    def set_material(self, material):
+        self._material = material
+
+    material = property(get_material, set_material)
+
+    def get_condition(self):
+        print('Retrieving condition')
+        return self._condition
+
+    def set_condition(self, condition):
+        self._condition = condition
+
+    condition = property(get_condition, set_condition)
+
+    def cobble(self):
+        self.condition = "New"
+        print('Your shoe is as good as new!')
